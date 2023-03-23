538e02de1eb8b05aac1df127d0274e9ecacda418
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-03-23 14:02:16 -0500

    More examples

--- Sources/DoccGPT/Instruction.swift
+++ Sources/DoccGPT/Instruction.swift
@@ -18,9 +18,7 @@ After:
 
  - Parameter recipient: The person being greeted.
 
- - Throws: `MyError.invalidRecipient`
-           if `recipient` is "Derek"
-           (he knows what he did).
+ - Throws: `MyError.invalidRecipient` if `recipient` is "Derek" (he knows what he did).
 
  - Returns: A new string saying hello to `recipient`.
  */
@@ -59,6 +57,41 @@ enum Style {
 
   /// A style for general-purpose transportation.
   case hybrid
+}
+```
+
+Please add Swift-flavored markdown comments to this code. Do not modify any of the code or any preexisting comments. Do not add any other types of comments, like file headers. Make sure that every single line of code is documented. Do not write new code. Only write new Swift-flavored markdown comments:
+
+Before:
+```
+struct EditParameters: Encodable {
+  var model: String
+  var input: String
+  var instruction: String
+  var temperature: Int
+  var topP: Int
+}
+```
+
+After:
+```
+/// A `struct` representing parameters for the edit endpoint
+struct EditParameters: Encodable {
+
+  /// ID of the model to use.
+  var model: String
+
+  /// The input text to use as a starting point for the edit.
+  var input: String
+
+  /// The instruction that tells the model how to edit the prompt.
+  var instruction: String
+
+  /// What sampling temperature to use, between 0 and 2
+  var temperature: Int
+
+  /// An alternative to sampling with temperature, called nucleus sampling
+  var topP: Int
 }
 ```
 
