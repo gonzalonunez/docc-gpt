26bd543fd2a44898f0628a4254afc7aab427ea3c
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-03-24 09:40:57 -0500

    Simplify folder structure, make some things public, update .spi.yml

--- Sources/Example/MovieWritingOptions.swift
+++ Sources/Example/MovieWritingOptions.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 /// Options for writing a movie.
-struct MovieWritingOptions {
+public struct MovieWritingOptions {
 
   /// The duration of the loop in seconds.
-  var loopDuration: TimeInterval
+  public var loopDuration: TimeInterval
 
   /// The duration of the movie in seconds. Defaults to 10.
-  var duration: Int = 10
+  public var duration: Int = 10
 
   /// Whether to overwrite existing files. Defaults to true.
-  var shouldOverwrite: Bool = true
+  public var shouldOverwrite: Bool = true
 }
--- Sources/Example/Post.swift
+++ Sources/Example/Post.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 /// A `struct` representing a post.
-struct Post: Codable {
+public struct Post: Codable {
 
   /// The post's unique identifier.
-  var id: String
+  public var id: String
 
   /// The date the post was created.
-  var createdAt: Date
+  public var createdAt: Date
 
   /// The date the post was last updated.
-  var updatedAt: Date
+  public var updatedAt: Date
 
   /**
    Creates a URL for the post.
@@ -19,7 +19,7 @@ struct Post: Codable {
 
    - Returns: A `URL` for the post, constructed from `baseURL` and `id`.
    */
-  func url(baseURL: String) -> URL? {
+  public func url(baseURL: String) -> URL? {
     return URL(string: "\(baseURL)/posts/\(id)")
   }
 }
