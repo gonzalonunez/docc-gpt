bbccacf9587426261c5a7210b6f630aad64315ea
gonzalonunez <gonzalonunez@users.noreply.github.com>
2023-04-14 11:43:09 +0000

    Run swift-format

--- Sources/Example/GifLoop.swift
+++ Sources/Example/GifLoop.swift
@@ -1,24 +1,24 @@
 #if canImport(ImageIO)
-import Foundation
-import ImageIO
+  import Foundation
+  import ImageIO
 
-/// An enumeration representing the looping behavior of a GIF.
-public enum GifLoop {
+  /// An enumeration representing the looping behavior of a GIF.
+  public enum GifLoop {
 
-  /// The GIF should loop a specific number of times.
-  case absolute(Int)
+    /// The GIF should loop a specific number of times.
+    case absolute(Int)
 
-  /// The GIF should loop infinitely.
-  public static var infinite = GifLoop.absolute(0)
+    /// The GIF should loop infinitely.
+    public static var infinite = GifLoop.absolute(0)
 
-  /// A dictionary representation of the loop behavior.
-  public var dict: [String: Int] {
-    switch self {
-    case .absolute(let loopCount):
-      return [
-        kCGImagePropertyGIFLoopCount as String: loopCount
-      ]
+    /// A dictionary representation of the loop behavior.
+    public var dict: [String: Int] {
+      switch self {
+      case .absolute(let loopCount):
+        return [
+          kCGImagePropertyGIFLoopCount as String: loopCount
+        ]
+      }
     }
   }
-}
 #endif
