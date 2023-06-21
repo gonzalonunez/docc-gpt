12940ffd072868522455b8dabe4738053f4bdd0e
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-06-21 15:36:38 -0700

    Add 16k models

--- Sources/docc-gpt/OpenAI.swift
+++ Sources/docc-gpt/OpenAI.swift
@@ -66,11 +66,13 @@ struct Model: ExpressibleByArgument {
   init?(argument: String) {
     self.id = argument
     switch id {
-    case "gpt-4", "gpt-4-0314":
+    case "gpt-4", "gpt-4-0613":
       contextLength = 8192
-    case "gpt-4-32k", "gpt-4-32k-0314":
+    case "gpt-4-32k", "gpt-4-32k-0613":
       contextLength = 32_768
-    case "gpt-3.5-turbo", "gpt-3.5-turbo-0301":
+    case "gpt-3.5-turbo", "gpt-3.5-turbo-0613":
+      contextLength = 4096
+    case "gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613":
       contextLength = 4096
     default:
       return nil
