bd7173e33fa8530f86baf576a55eccb702eeb727
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-06-21 15:37:41 -0700

    Update context lengths

--- Sources/docc-gpt/OpenAI.swift
+++ Sources/docc-gpt/OpenAI.swift
@@ -67,13 +67,13 @@ struct Model: ExpressibleByArgument {
     self.id = argument
     switch id {
     case "gpt-4", "gpt-4-0613":
-      contextLength = 8192
+      contextLength = 8_192
     case "gpt-4-32k", "gpt-4-32k-0613":
       contextLength = 32_768
     case "gpt-3.5-turbo", "gpt-3.5-turbo-0613":
-      contextLength = 4096
+      contextLength = 4_096
     case "gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613":
-      contextLength = 4096
+      contextLength = 16_384
     default:
       return nil
     }
